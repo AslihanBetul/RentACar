4377ce4d40fabcfcd80230ab2a1b0dabbcd06721
aslihanbetul <aslihan.mert19@gmail.com>
2024-07-20 22:07:08 +0300

    20-07 -gün sonu

--- MailService/src/main/java/com/abm/service/EmailService.java
+++ MailService/src/main/java/com/abm/service/EmailService.java
@@ -50,64 +50,56 @@ public class EmailService {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
 
-        String htmlContent = "<html><body>" +
-                "<table cellpadding=\"0\" cellspacing=\"0\" align=\"center\" width=\"770px\" style=\"font-family:Arial,sans-serif;color:#000000;background-color:#f8f8f8;font-size:14px;\">\n" +
-                "    <tbody><tr>\n" +
-                "        <td style=\"padding-top:60px;padding-right:70px;padding-bottom:60px;padding-left:70px\">\n" +
-                "            <table width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" align=\"center\" style=\"border-color:#e6e6e6;border-width:1px;border-style:solid;background-color:#fff;padding-top:25px;padding-right:0;padding-bottom:50px;padding-left:30px\">\n" +
-                "                <tbody><tr>\n" +
-                "                    <td style=\"padding:0 30px 30px\">\n" +
-                "                        <p style=\"font-family:Arial,sans-serif;color:#000000;font-size:19px;margin-top:14px;margin-bottom:0\">\n" +
-                "                            Merhaba, " + userDto.getBody().getName() + " !\n" +
-                "                        </p>\n" +
-                "                        <p style=\"font-family:Arial,sans-serif;color:#000000;font-size:14px;line-height:17px;margin-top:30px;margin-bottom:0\">\n" +
-                "                           Kiralanan araç bilgileri aşağıdadır\n" +
-                "                        </p>\n" +
-                "                        <ul style=\"font-family:Arial,sans-serif;color:#000000;font-size:14px;line-height:17px;margin-top:10px;margin-bottom:0\">\n" +
-                "                            <li style=\"font-family:Arial,sans-serif;color:#000000;font-size:14px;line-height:17px;margin-top:0;margin-bottom:5px\">\n" +
-                "                                <b> " +
-                "                            </li>\n" +
-                "                        </ul>\n" +
-                "                        <ul style=\"font-family:Arial,sans-serif;color:#000000;font-size:14px;line-height:17px;margin-top:10px;margin-bottom:0\">\n" +
-                "                            <li style=\"font-family:Arial,sans-serif;color:#000000;font-size:14px;line-height:17px;margin-top:0;margin-bottom:5px\">\n" +
 
-                 "             <img style=\"width: 500px;\" src=\" https://www.altexsoft.com/static/blog-post-featured/2023/10/4fae8217-b4ac-49b4-b02a-91c243524301.jpg"  +
-                "                                <b>Name: </b>" + model.getName() + "<br>\n" +
-                "                                <b>Model: </b>" + model.getModel() + "<br>\n" +
-                "                                <b>Brand: </b>" + model.getBrand() + "<br>\n" +
-                "                                <b>Plate: </b>" + model.getPlate() + "<br>\n" +
-                "                                <b>Rent Date: </b>" + model.getRentDate() + "<br>\n" +
-                "                                <b>Return Date: </b>" + model.getReturnDate() + "<br>\n" +
-                "                                <b>Total Price: ₺</b>" + model.getTotalPrice() + "<br>\n" +
-                "                                <b>Car Image: </b><img src=\"" + model.getImageUrl() + "\" alt=\"Car Image\">\n" +
-                "                            </li>\n" +
-                "                        </ul>\n" +
-                "                    </td>\n" +
-                "                </tr>\n" +
-                "            </tbody></table>\n" +
-                "            <table width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" align=\"center\">\n" +
-                "                <tbody><tr>\n" +
-                "                    <td style=\"padding-top:12px;\"></td>\n" +
-                "                </tr>\n" +
-                "                <tr>\n" +
-                "                    <td style=\"font-family:Arial,sans-serif;font-size:12px;color:#888888;padding-right:30px;padding-left:30px\">\n" +
-                "                        Lütfen bu e-postayı yanıtlamayın. Beeme Destek Ekibi'ne ulaşmak için <a href=\"https://beemeapp.com/iletisim.html\" target=\"_blank\">iletişim formunu</a> kullanın.\n" +
-                "                    </td>\n" +
-                "                </tr>\n" +
-                "            </tbody></table>\n" +
-                "        </td>\n" +
-                "    </tr></tbody></table> </body></html>";
+        String htmlContent = "<html><body>" +
+                "<table cellpadding=\"0\" cellspacing=\"0\" align=\"center\" width=\"770px\" style=\"font-family:Arial,sans-serif;color:#000000;background-color:#f8f8f8;font-size:14px;\">" +
+                "    <tbody><tr>" +
+                "        <td style=\"padding-top:60px;padding-right:70px;padding-bottom:60px;padding-left:70px\">" +
+                "            <table width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" align=\"center\" style=\"border-color:#e6e6e6;border-width:1px;border-style:solid;background-color:#fff;padding-top:25px;padding-right:0;padding-bottom:50px;padding-left:30px\">" +
+                "                <tbody><tr>" +
+                "                    <td style=\"padding:0 30px 30px\">" +
+                "                        <p style=\"font-family:Arial,sans-serif;color:#000000;font-size:19px;margin-top:14px;margin-bottom:0\">" +
+                "                            Merhaba, " + userDto.getBody().getName() + " !" +
+                "                        </p>" +
+                "                        <p style=\"font-family:Arial,sans-serif;color:#000000;font-size:14px;line-height:17px;margin-top:30px;margin-bottom:0\">" +
+                "                           Kiralanan araç bilgileri aşağıdadır" +
+                "                        </p>" +
+                "                        <ul style=\"font-family:Arial,sans-serif;color:#000000;font-size:14px;line-height:17px;margin-top:10px;margin-bottom:0\">" +
+                "                            <li style=\"font-family:Arial,sans-serif;color:#000000;font-size:14px;line-height:17px;margin-top:0;margin-bottom:5px\">" +
+                "                                <b>Name: </b>" + model.getName() + "<br>" +
+                "                                <b>Model: </b>" + model.getModel() + "<br>" +
+                "                                <b>Brand: </b>" + model.getBrand() + "<br>" +
+                "                                <b>Plate: </b>" + model.getPlate() + "<br>" +
+                "                                <b>Rent Date: </b>" + model.getRentDate() + "<br>" +
+                "                                <b>Return Date: </b>" + model.getReturnDate() + "<br>" +
+                "                                <b>Total Price: ₺</b>" + model.getTotalPrice() + "<br>" +
+                "                                <b>Car Image: </b><img src=\"" + model.getImageUrl() + "\" alt=\"Car Image\" style=\"width: 300px;\"/>" +
+                "                            </li>" +
+                "                        </ul>" +
+                "                    </td>" +
+                "                </tr>" +
+                "            </tbody></table>" +
+                "            <table width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" align=\"center\">" +
+                "                <tbody><tr>" +
+                "                    <td style=\"padding-top:12px;\"></td>" +
+                "                </tr>" +
+                "                <tr>" +
+                "                    <td style=\"font-family:Arial,sans-serif;font-size:12px;color:#888888;padding-right:30px;padding-left:30px\">" +
+                "                        Lütfen bu e-postayı yanıtlamayın. Beeme Destek Ekibi'ne ulaşmak için <a href=\"https://beemeapp.com/iletisim.html\" target=\"_blank\">iletişim formunu</a> kullanın." +
+                "                    </td>" +
+                "                </tr>" +
+                "            </tbody></table>" +
+                "        </td>" +
+                "    </tr></tbody></table>" +
+                "</body></html>";
+
+
         helper.setText(htmlContent, true);
         helper.setTo(userDto.getBody().getEmail());
-        helper.setSubject("your activation code");
+        helper.setSubject("Kiralanan Araç Bilgileri");
+
 
         mailSender.send(mimeMessage);
-
-
-
-
-
-
     }
 
     public void sendBulkHtmlEmail() throws MessagingException {
